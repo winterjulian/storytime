150e33ee606c67a644698ddcb0cc0f32b23e7c88
Julian Winter <julian.winter91@gmail.com>
2025-06-29 19:16:06 +0200

    Add styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        'inner-strong': 'inset 0 4px 6px rgba(0,0,0,0.6)',
+        'inner-xl': 'inset 0 8px 12px rgba(0,0,0,0.8)',
+      },
+    },
   },
   plugins: [],
 }
