23d4c40f31914dbf0ddbeff6c2f2d1db1db51092
Julian Winter <julian.winter91@gmail.com>
2025-06-30 21:15:20 +0200

    Reduce user-step-element

--- src/app/components/user-step-element/user-step-element.ts
+++ src/app/components/user-step-element/user-step-element.ts
@@ -1,7 +1,6 @@
-import {Component, inject, input, OnInit, output} from '@angular/core';
+import {Component, input, OnInit, output} from '@angular/core';
 import {UserStep} from '../../interfaces/user-step';
 import {UserJourney} from '../../interfaces/user-journey';
-import {DragDropService} from '../../services/drag-drop.service';
 
 @Component({
   selector: 'app-user-step-element',
@@ -9,23 +8,8 @@ import {DragDropService} from '../../services/drag-drop.service';
   templateUrl: './user-step-element.html',
   styleUrl: './user-step-element.scss',
 })
-export class UserStepElement implements OnInit {
-  public dragDropService = inject(DragDropService);
-
+export class UserStepElement {
   public userStep = input.required<UserStep>();
   public userJourney = input.required<UserJourney>();
-  public dropZones = this.dragDropService.connectedDropZones;
   public delete = output<void>();
-
-  public dropZoneId = '';
-
-  ngOnInit() {
-    this.dropZoneId =
-      'drop-' + this.userJourney().id + '-' + this.userStep().id;
-    this.dragDropService.registerDropZone(this.dropZoneId);
-    this.dragDropService.registerContainer(
-      this.dropZoneId,
-      this.userStep().issues,
-    );
-  }
 }
